b3b5d1d03648787077fdf19de420807f77e6d86c
gcrth <gcrth@example.com>
2019-05-28 19:14:59 +0800

    update python script

--- emailSend.py
+++ emailSend.py
@@ -22,19 +22,19 @@ class Email:
 
         self.smtp_obj = smtplib.SMTP_SSL(self.HOST, self.PORT)
         res = self.smtp_obj.login(user=self.FROM, password='systemMonitor123')
-        print(res, '登录成功')
+        print(res, 'login success')
 
     def send(self, msg):
         message = MIMEText(msg, 'plain', 'utf-8')
         message['From'] = self.FROM
         message['To'] = self.TO[0]
-        message['Subject'] = Header('进程状态提醒', 'utf-8')
+        message['Subject'] = Header('Process state change reminder', 'utf-8')
 
         try:
             self.smtp_obj.sendmail(self.FROM, self.TO, message.as_string())
-            print('发送成功')
+            print('send success')
         except Exception:
-            print('发送失败')
+            print('failure')
 
 
 if __name__ == "__main__":
